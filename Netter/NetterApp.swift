ad4e7b3239b47307d3e7cda29d71d95d31729ff3
Andreas <andreas@goproactive.se>
2026-03-11 10:21:45 +0100

    Initial Commit

---
+++ Netter/NetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetterApp.swift
+//  Netter
+//
+//  Created by Andreas Hanfelt on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct NetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
